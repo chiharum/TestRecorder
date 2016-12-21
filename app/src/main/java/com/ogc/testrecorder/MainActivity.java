410860f37db3dcea473caf825957777d300b4ea2
chiharum <m.chiharu0611@gmail.com>
2016-12-21 19:01:42 +0900

    Completed, but still working on.

--- app/src/main/java/com/ogc/testrecorder/MainActivity.java
+++ app/src/main/java/com/ogc/testrecorder/MainActivity.java
@@ -1,14 +1,19 @@
 package com.ogc.testrecorder;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     static final String Intent_bookName = "bookName";
 
     String bookTitles[];
+    int recordsCounts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
         mySQLiteOpenHelper = new MySQLiteOpenHelper(getApplicationContext());
         database = mySQLiteOpenHelper.getWritableDatabase();
 
-        int countOfBooks = countBooks();
-        if(countOfBooks == 0){
-            insertNewBook("book1");
+        recordsCounts = countBooks();
+        if(recordsCounts == 0){
+            editBookTitle();
         }
 
-        bookTitles = new String[countOfBooks];
+        bookTitles = new String[recordsCounts];
 
         listView = (ListView)findViewById(R.id.listView);
         setListView();
@@ -51,13 +57,12 @@ public class MainActivity extends AppCompatActivity {
         items = new ArrayList<>();
 
         String title;
-        int n = 0;
         bookTitles = searchBookTitle();
-        while(bookTitles[n] != null){
+        int n;
+        for(n = 0; n < recordsCounts; n++){
             title = bookTitles[n];
             listItem item = new listItem(title);
             items.add(item);
-            n++;
         }
 
         customAdapter = new listCustomAdapter(this, R.layout.main_listview_layout, items);
@@ -65,20 +70,38 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, CountActivity.class);
-                intent.putExtra(Intent_bookName, bookTitles[position]);
-                startActivity(intent);
+
+                final int listPosition = position;
+
+                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                alertDialogBuilder.setTitle("オプション");
+                alertDialogBuilder.setItems(getResources().getStringArray(R.array.book_choosing_dialog_menus), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == 0) {
+                            //開く
+                            Intent intent = new Intent();
+                            intent.setClass(MainActivity.this, CountActivity.class);
+                            intent.putExtra(Intent_bookName, bookTitles[listPosition]);
+                            startActivity(intent);
+
+                        } else if (which == 1) {
+                            //編集
+                            editBookTitle();
+
+                        } else if (which == 2) {
+                            //削除
+                        }
+                    }
+                });
             }
         });
     }
 
     public String[] searchBookTitle(){
 
-        int recordsAmount = countBooks();
-
         Cursor cursor = null;
-        String[] result = new String[recordsAmount];
+        String[] result = new String[recordsCounts];
 
         try{
             cursor = database.query(MySQLiteOpenHelper.BooksTableName, new String[]{MySQLiteOpenHelper.Table_bookName}, null, null, null, null, null);
@@ -105,12 +128,45 @@ public class MainActivity extends AppCompatActivity {
         database.insert(MySQLiteOpenHelper.BooksTableName, null, values);
     }
 
+    public void updateBook(int position, String title){
+
+    }
+
     public int countBooks(){
         return (int)DatabaseUtils.queryNumEntries(database, MySQLiteOpenHelper.BooksTableName);
     }
 
+    public void editBookTitle(boolean isNew, int position){
+
+        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View alertDialogLayout = layoutInflater.inflate(R.layout.edit_text_dialog_layout, null);
+
+        final EditText editText = (EditText)alertDialogLayout.findViewById(R.id.editBookTitleEditText);
+
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("本の名前を入力してください");
+        alertDialog.setView(alertDialogLayout);
+        alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String text;
+
+                SpannableStringBuilder spannableStringBuilder = (SpannableStringBuilder) editText.getText();
+                if (spannableStringBuilder == null) {
+                    text = null;
+                } else {
+                    text = spannableStringBuilder.toString();
+                }
+                insertNewBook(text);
+                recordsCounts++;
+            }
+        });
+        alertDialog.setNegativeButton("キャンセル", null);
+        alertDialog.show();
+    }
+
     public void newBook(View view){
-        int amountOfBooks = countBooks();
-        insertNewBook("book" + (amountOfBooks + 1));
+        editBookTitle();
     }
 }
--- app/src/main/java/com/ogc/testrecorder/MySQLiteOpenHelper.java
+++ app/src/main/java/com/ogc/testrecorder/MySQLiteOpenHelper.java
@@ -14,6 +14,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
     static final String Table_id = "id";
     static final String Table_bookName = "book_name";
+    static final String Table_bookId = "book_id";
     static final String ContentsTable_questionNumber = "question_number";
     static final String ContentsTable_timesChallenged = "times_challenged";
     static final String ContentsTable_timesCorrect = "times_correct";
@@ -23,8 +24,8 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     }
 
     public void onCreate(SQLiteDatabase database){
-        database.execSQL("create table " + BooksTableName + " (" + Table_id + " integer primary key autoincrement not null, " + Table_bookName + " string not null)");
-        database.execSQL("create table " + ContentsTableName + " (" + Table_id + " integer primary key autoincrement not null, " + Table_bookName + " string not null, " + ContentsTable_questionNumber + " integer not null, " + ContentsTable_timesChallenged + " integer not null, " + ContentsTable_timesCorrect + " integer not null)");
+        database.execSQL("create table " + BooksTableName + " (" + Table_id + " integer primary key autoincrement not null, " + Table_bookName + " string not null, " + Table_bookId + " integer not null)");
+        database.execSQL("create table " + ContentsTableName + " (" + Table_id + " integer primary key autoincrement not null, " + Table_bookName + " string not null, " + Table_bookId + " integer not null, " + ContentsTable_questionNumber + " integer not null, " + ContentsTable_timesChallenged + " integer not null, " + ContentsTable_timesCorrect + " integer not null)");
     }
 
     public void onUpgrade (SQLiteDatabase database, int oldVersion, int newVersion){
